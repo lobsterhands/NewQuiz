f03288b2bd29f2b70b83feab27273a83bf518bc6
lobsterhands <denmanlp@gmail.com>
2015-01-18 15:39:38 -0600

    Added question constructor function; Updated getSelected(); overhauled #btn-next.on('click') in a big way

--- js/main.js
+++ js/main.js
@@ -2,53 +2,28 @@ var $,
     i,
     j,
     k,
+    question,
     questions,
     answerKey,
     userAnswer,
     selected,
     score;
 
+question = function(question, choices, answer) {
+    return {question:question, choices:choices, answer:answer, answered:false};
+};
+
 questions = [
-    {question: "What is my middle name?",
-    choices: ["Sylvester", "Patrick", "Aaron", "David"],
-    answer:1
-    },
-    {question: "How old am I?",
-    choices: ["29", "27", "30", "28"],
-    answer:0
-    },
-    {question: "In what state was I born?",
-    choices: ["Idaho", "Tampa", "North Carolina", "California"],
-    answer:3
-    },
-    {question: "I served in this branch of the military:",
-    choices: ["Air Force", "Army", "Navy", "Marines"],
-    answer:0
-    },
-    {question: "This is my nervous habit:",
-    choices: ["Blinking too much", "Poor eye contact", "Cracking knuckles", "I'm never nervous"],
-    answer:2
-    },
-    {question: "What is my trail name?",
-    choices: ["Landlocked", "Ambassador", "Moonbeam", "Young Man Winter"],
-    answer:1
-    },
-    {question: "How many siblings do I have?",
-    choices: ["0", "1", "3", "4"],
-    answer:3
-    },
-    {question: "All of these are my hobbies except:",
-    choices: ["Stamp collecting", "Playing guitar", "Programming", "Board Games"],
-    answer:0
-    },
-    {question: "What is my favorite movie?",
-    choices: ["Titanic", "The Matrix", "The Royal Tenenbaums", "Inception"],
-    answer:2
-    },
-    {question: "What is my favorite work of fiction?",
-    choices: ["A Wizard of Earthsea", "Harry Potter (all of them)", "The Lord of the Rings", "The Bible"],
-    answer:0
-    }
+    question("What is my middle name?", ["Sylvester", "Patrick", "Aaron", "David"], 1),
+    question("How old am I?", ["29", "27", "30", "28"], 0),
+    question("In what state was I born?", ["Idaho", "Tampa", "North Carolina", "California"], 3),
+    question("I served in this branch of the military:",["Air Force", "Army", "Navy", "Marines"], 0),
+    question("This is my nervous habit:", ["Blinking too much", "Poor eye contact", "Cracking knuckles", "I'm never nervous"], 2),
+    question("What is my trail name?", ["Landlocked", "Ambassador", "Moonbeam", "Young Man Winter"], 1),
+    question("How many siblings do I have?", ["0", "1", "3", "4"], 3),
+    question("All of these are my hobbies except:", ["Stamp collecting", "Playing guitar", "Programming", "Board Games"], 0),
+    question("What is my favorite movie?", ["Titanic", "The Matrix", "The Royal Tenenbaums", "Inception"], 2),
+    question("What is my favorite work of fiction?", ["A Wizard of Earthsea", "Harry Potter (all of them)", "The Lord of the Rings", "The Bible"], 0)
 ];
 
 answerKey = [];
@@ -60,48 +35,63 @@ questions.forEach(function(element){
     answerKey.push(element.answer);
 });
 
+function getSelected() {
+    if (!questions[i].answered) {
+        $('#quiz-form').on('change', function () {
+            selected = $("input[name=question]:checked").val();
+            $("#btn-next").show();
+        });
+    }
+}
+
 $(document).ready( function() {
 
     var quiz = $("#quiz");
+    var ch0 = $('#choice0');
+    var ch1 = $('#choice1');
+    var ch2 = $('#choice2');
+    var ch3 = $('#choice3');
 
-    function getSelected() {
-        selected = false;
-        $('#quiz-form').on('change', function() {
-            selected = ($('input[name=question]:checked').val());
-        });
-    }
     getSelected();
-
-    // @lob Keep selected answers in memory to be shown when Prev button is used; currently,
-    // it resets it to 'checked', false
-
-    // @lob Add checkbox verification; Next button only advances if an answer is selected;
-    // otherwise, alert error
-
-    // @lob Add missed questions review after grade is shown
-
-    // @lob Fix navigation history; if userAnswer[i] exists, that radio checkbox should be checked;
-    // Currently, if you go back to a question but don't change the radio selection, the
-    // 'please select an answer' msg is displayed
 
     // @Lob add 'are you sure' button before the end of the quiz.
 
     // @Lob add 'reset' button after grade is shown
 
-var turd = true;
+    // @lob Enable an on('change') of a new value to update the userAnswer[i]; currently not working
+
     $('#btn-next').on('click', function() {
+        if (!questions[i].answered) {
+            userAnswer.splice(i, 1, selected);
+            questions[i].answered = true;
+        }
         if (i < answerKey.length-1) {
-            if (selected) {
-                userAnswer.splice(i, 1, selected);
-                i += 1;
-                quiz.find('.quiz-question').show().html('<br>' + '<h2>' + questions[i].question + '</h2>');
-                $('#choice0').prop('checked', false).next().html('<strong>' + questions[i].choices[0] + '</strong>');
-                $('#choice1').prop('checked', false).next().html('<strong>' + questions[i].choices[1] + '</strong>');
-                $('#choice2').prop('checked', false).next().html('<strong>' + questions[i].choices[2] + '</strong>');
-                $('#choice3').prop('checked', false).next().html('<strong>' + questions[i].choices[3] + '</strong>');
-            } else {
-                alert('Please select an answer.');
+            i += 1;
+            quiz.find('.quiz-question').show().html('<br>' + '<h2>' + questions[i].question + '</h2>');
+            ch0.prop('checked', false).next().html('<strong>' + questions[i].choices[0] + '</strong>');
+            ch1.prop('checked', false).next().html('<strong>' + questions[i].choices[1] + '</strong>');
+            ch2.prop('checked', false).next().html('<strong>' + questions[i].choices[2] + '</strong>');
+            ch3.prop('checked', false).next().html('<strong>' + questions[i].choices[3] + '</strong>');
+            switch (userAnswer[i]) {
+                case '0':
+                    $('#choice0').prop('checked', true);
+                    break;
+                case '1':
+                    ch1.prop('checked', true);
+                    break;
+                case '2':
+                    ch2.prop('checked', true);
+                    break;
+                case '3':
+                    ch3.prop('checked', true);
+                    break;
+                default:
+                    break;
             }
+            if (!questions[i].answered) {
+                $("#btn-next").hide();
+            }
+            $("#btn-prev").show();
         } else {
             userAnswer.push(selected);
             quiz.find('.quiz-question').hide();
@@ -110,36 +100,42 @@ var turd = true;
             $("#btn-next").hide();
             $("#btn-prev").hide();
             $("#btn-submit").show();
-            }
+        }
         getSelected();
     });
 
 
     $('#btn-prev').on('click', function() {
-        if (i > 0) {
+        if (i >= 0) {
             i -= 1;
+            if (questions[i].answered) {
+                $("#btn-next").show();
+            }
             quiz.find('.quiz-question').show().html('<br>' + '<h2>' + questions[i].question + '</h2>');
-            $('#choice0').next().html('<strong>' + questions[i].choices[0] + '</strong>');
-            $('#choice1').next().html('<strong>' + questions[i].choices[1] + '</strong>');
-            $('#choice2').next().html('<strong>' + questions[i].choices[2] + '</strong>');
-            $('#choice3').next().html('<strong>' + questions[i].choices[3] + '</strong>');
+            ch0.next().html('<strong>' + questions[i].choices[0] + '</strong>');
+            ch1.next().html('<strong>' + questions[i].choices[1] + '</strong>');
+            ch2.next().html('<strong>' + questions[i].choices[2] + '</strong>');
+            ch3.next().html('<strong>' + questions[i].choices[3] + '</strong>');
+            if (i < 1) {
+                $("#btn-prev").hide()
+            }
         }
 
         switch (userAnswer[i]) {
             case '0':
-                $('#choice0').prop('checked', true);
+                ch0.prop('checked', true);
                 break;
             case '1':
-                $('#choice1').prop('checked', true);
+                ch1.prop('checked', true);
                 break;
             case '2':
-                $('#choice2').prop('checked', true);
+                ch2.prop('checked', true);
                 break;
             case '3':
-                $('#choice3').prop('checked', true);
+                ch3.prop('checked', true);
                 break;
         }
-
+        getSelected();
     });
 
     $('#btn-submit').on('click', function() {
@@ -155,6 +151,7 @@ var turd = true;
         $("#grade-percent").text((score/(answerKey.length))*100 + '%');
 
         // @lob Use a switch instead of if/else
+        // @lob Change score to percentage based (100%, >= 90%, etc.)
 
         if (score === 10) {
             $("#ten").show();
@@ -183,8 +180,10 @@ var turd = true;
 
     quiz.find('.quiz-choices').show();
     quiz.find('.quiz-question').show().html('<br>' + '<h2>' + questions[i].question + '</h2>');
-    $('#choice0').prop('checked', false).next().html('<strong>'+questions[i].choices[0]+'</strong>');
-    $('#choice1').prop('checked', false).next().html('<strong>'+questions[i].choices[1]+'</strong>');
-    $('#choice2').prop('checked', false).next().html('<strong>'+questions[i].choices[2]+'</strong>');
-    $('#choice3').prop('checked', false).next().html('<strong>'+questions[i].choices[3]+'</strong>');
+    ch0.prop('checked', false).next().html('<strong>'+questions[i].choices[0]+'</strong>');
+    ch1.prop('checked', false).next().html('<strong>'+questions[i].choices[1]+'</strong>');
+    ch2.prop('checked', false).next().html('<strong>'+questions[i].choices[2]+'</strong>');
+    ch3.prop('checked', false).next().html('<strong>'+questions[i].choices[3]+'</strong>');
 });
+
+// @lob Add missed questions review after grade is shown
